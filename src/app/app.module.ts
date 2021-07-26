92b2a46c294fe28a8800f5a6f01e36bc27dedbcf
Tonatiuj Sanchez <tonsanzjimz@gmail.com>
2021-07-25 20:25:26 -0500

    Diseño terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { InputComponent } from './components/input/input.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { MainComponent } from './main/main.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    InputComponent,
+    SpinnerComponent,
+    MainComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
